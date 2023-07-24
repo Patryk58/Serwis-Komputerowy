7aa580db75f9944a885f92016a2c839faf99db24
Patryk58 <140334952+Patryk58@users.noreply.github.com>
2023-07-24 08:55:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeimg(x)
+{
+	document.getElementById("img_change1").src = "Zdjecia/Uslugi/u"+ x +"1.png";
+	document.getElementById("img_change2").src = "Zdjecia/Uslugi/u"+ x +"2.png";
+	document.getElementById("img_change3").src = "Zdjecia/Uslugi/u"+ x +"3.png";
+	document.getElementById("img_change4").src = "Zdjecia/Uslugi/u"+ x +"4.png";
+}
